b201d4937a168bf45011f0a10e7c645072d4ad38
Daniel <81879122+DanielPinhasov@users.noreply.github.com>
2022-11-13 12:25:51 +0200

    Initial Commit

---
+++ slotGameDemo/slotGameDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  slotGameDemoApp.swift
+//  slotGameDemo
+//
+//  Created by דניאל פינחסוב on 13/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct slotGameDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
